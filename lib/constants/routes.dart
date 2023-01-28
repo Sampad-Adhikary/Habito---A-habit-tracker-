e4018e82209c1767831acf71ff61b9b83dab8a48
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-28 11:45:54 +0530

    added files and folders

---
+++ lib/constants/routes.dart
@@ -0,0 +1,14 @@
+class Routes {
+  static const String onboardingScreen1 = "/onboarding1";
+  static const String onboardingScreen2 = "/onboarding2";
+  static const String onboardingScreen3 = "/onboarding3";
+  static const String loginScreen = "/log-in";
+  static const String otpScreen = "/verify-otp";
+  static const String signUpSCreen = "/sign-up";
+  static const String homeScreen = "/home";
+  static const String addHabitsScreen = "/addHabits";
+  static const String settingsScreen = "/settings";
+  static const String profileScreen = "/edit-profile";
+  static const String analyticsScreen = "/analytics";
+  static const String streaksScreen = "/streaks";
+}
