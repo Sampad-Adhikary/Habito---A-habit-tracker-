f06859f6d0ed3ef38abb854278f8dfb344c8edf6
deepakk14 <deepakk14.06.01@gmail.com>
2023-01-30 20:01:53 +0530

    commit 1

--- lib/screens/addHabitsScreen.dart
+++ lib/screens/addHabitsScreen.dart
@@ -1,15 +1,194 @@
 import 'package:flutter/material.dart';
-import 'package:habito/constants/routes.dart';
+import 'package:habito/services/lists.dart';
 
-class Habits extends StatelessWidget {
-  const Habits({super.key});
+
+
+class AddHabits extends StatefulWidget {
+  const AddHabits({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Add Habits"),
-      ),
-    );
-  }
+  State<AddHabits> createState() => _AddHabitsState();
 }
+
+class _AddHabitsState extends State<AddHabits> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    void addToHabitList(String habitName, String habitDescription) {
+      habitList.add([false, habitName, habitDescription, Icon(Icons.abc)]);
+    }
+
+    void addHabit(BuildContext context) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            TextEditingController habitNameController =
+                TextEditingController(text: "Habit Name");
+            TextEditingController habitDescriptionController =
+                TextEditingController(text: "Habit Description");
+            return AlertDialog(
+              title: Text("Add a Habit"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: habitNameController,
+                  ),
+                  TextFormField(
+                    controller: habitDescriptionController,
+                  ),
+                ],
+              ),
+              actions: [
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.deepPurple),
+                        surfaceTintColor:
+                            MaterialStateProperty.all(Colors.purpleAccent)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel")),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.deepPurple),
+                        surfaceTintColor:
+                            MaterialStateProperty.all(Colors.purpleAccent)),
+                    onPressed: () {
+                      setState(() {
+                        addToHabitList(habitNameController.text,
+                            habitDescriptionController.text);
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Text("Save"))
+              ],
+            );
+          });
+    }
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        mini: true,
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
+        onPressed: (() {
+          addHabit(context);
+        }),
+      ),
+      backgroundColor: Colors.grey[100],
+      body: ListView(padding: EdgeInsets.zero, children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Stack(children: [
+              Image.asset('assets/habits.png'),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 140, 0, 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(children: [
+                    Text(
+                      "Hey Hermano!",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "You have ${habitList.length - counter} habits left for today",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),
+                    ),
+                  ]),
+                ),
+              )
+            ]),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Keep Going!",
+                        style: TextStyle(color: Colors.grey[600], fontSize: 16),
+                      ),
+                      Text("${((counter / habitList.length) * 100).round()}%",
+                          style:
+                              TextStyle(color: Colors.grey[600], fontSize: 16))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    child: LinearProgressIndicator(
+                        minHeight: 12,
+                        color: Colors.deepPurpleAccent,
+                        backgroundColor: Color.fromARGB(255, 192, 170, 250),
+                        value: (counter / habitList.length)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Divider(),
+                ),
+                SizedBox(
+                  child: ListView.builder(
+                    
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    itemCount: habitList.length,
+                    itemBuilder: (context, int index) {
+                      return ListTile(
+                          title: Text(habitList[index][1],style: TextStyle(color: Colors.black),),
+                          subtitle: Text(habitList[index][2],style: TextStyle(color: Colors.black)),
+                          trailing: habitList[index][3],
+                          leading: Checkbox(
+                            side: BorderSide(color: Colors.grey,width: 1.5),
+                            checkColor: Colors.black,
+                            value: habitList[index][0],
+                            onChanged: ((value) {
+                              setState(() {
+                                if (value == false) {
+                                  counter -= 1;
+                                  print(counter.toString());
+                                  habitList[index][0] = value;
+                                } else
+                                  counter += 1;
+                                print(counter.toString());
+                                habitList[index][0] = value;
+                              });
+                            }),
+                          ));
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }}
