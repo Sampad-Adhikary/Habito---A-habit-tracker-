672b647dd3bd502a3254d3ad0caa4baded886f29
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-02-04 11:36:18 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,6 @@ class Habito extends StatelessWidget {
           "/": (context) => const SplashScreen(),
           // "/": (context) => AppBar(),
           Routes.onboardingScreen1: (context) => Onboarding(),
-          // Routes.onboardingScreen2: (context) => Onboarding1(),
-          // Routes.onboardingScreen3: (context) => Onboarding1(),
           Routes.loginScreen: (context) => Login(),
           Routes.otpScreen: (context) => Otp(),
           Routes.signUpSCreen: (context) => Signup(),
